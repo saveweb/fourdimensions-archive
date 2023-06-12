e4c4bc50a2ac3146d28b25cca5d808577b371bea
yzqzss <yzqzss@yandex.com>
2023-06-13 03:21:35 +0800

    webapi.grou.getGroupDetail: 错误处理；完善示例

--- fourdimensions/webapi/group/getGroupDetail.py
+++ fourdimensions/webapi/group/getGroupDetail.py
@@ -1,22 +1,32 @@
 # API https://bcy.net/apiv3/group/getGroupDetail
 
+import json
 import requests
 from typing import List
 
+from fourdimensions.webapi.const import DEFAULT_HEADER
+
 class getGroupDetail:
+    class ItemNotFoundError(Exception):
+        pass
+
     @staticmethod
     def get(gid:int, sess: requests.Session = None):
         """ query https://bcy.net/apiv3/group/getGroupDetail
         
         Args:
             gid: 话题id
-            sess: requests创建的session，此处需要用到其中的_csrf_token鉴权，所以必须填写。
 
+        Returns:
+            r.json()
+
+
+        Raises:
+            getGroupDetail.ItemNotFoundError: item不存在或暂时不可见。一般是 gid 不存在
         """
 
         # session刚创建时，其中不会有用于鉴权的_csrf_token。
         # 所以此处需要多访问一步原始的话题(group)网页，获取_csrf_token。
-
         if "_csrf_token" not in sess.cookies:
             r = sess.get(f"https://bcy.net/group/list/{gid}")
         _csrf_token = sess.cookies["_csrf_token"]
@@ -30,9 +40,23 @@ class getGroupDetail:
         r = sess.post(url, data=data)
         r.raise_for_status()
 
+        response_json: dict = r.json()
+        if response_json.get('code', -1) == 500001 and response_json.get('msg', '') == "item不存在或暂时不可见":
+            raise getGroupDetail.ItemNotFoundError(response_json.get('msg'))
+        assert response_json.get('code', -1) == 0, response_json.get('msg')
+
         return r.json()
 
 if __name__ == "__main__":
     sess = requests.session()
-    reply = getGroupDetail.get(763842, sess=sess)
-    print(reply)
+    sess.headers.update(DEFAULT_HEADER)
+    for gid in [763842, 7638420000]: # 第一个存在，第二个不存在
+        try:
+            print('gid:', gid)
+            group_detail = getGroupDetail.get(gid, sess=sess)
+        except getGroupDetail.ItemNotFoundError as e:
+            print(str(e))
+        else:
+            with open(__file__.replace(__file__.split("/")[-1], "group_detail-demo.json"), "w", encoding="utf-8") as f:
+                json.dump(group_detail, f, indent=4, ensure_ascii=False)
+            print('成功')
